7d10d3aeed3ba142dab8d0a90a4440e051faf8a9
Md Saif <your_email@gmail.com>
2026-05-24 04:14:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Md_Saif",
+    author_email="itssaif5@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
